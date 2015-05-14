f0d852229a8652573f6e4a91a491e4091e243fa4
Daniel Hurley <dg.hurley@gmail.com>
2015-05-14 19:40:20 +1000

    Rewrote the package installation script to also get the R packages

--- scripts/install_budden2015treeome_required_packages.sh
+++ scripts/install_budden2015treeome_required_packages.sh
@@ -1,13 +1,15 @@
-log=/vagrant/temp/install_gawcurcra15.log
+log=/vagrant/temp/install_budden2015treeome.log
 
-echo Installing packages required for gawcurcra15 | tee -a $log
+echo Installing packages required for budden2015treeome | tee -a $log
 
-wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu precise-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
+#wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
+#sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu precise-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
 
 sudo apt-get -y update 
-sudo apt-get -y install octave xfig reduce-algebra evince epstool
+sudo apt-get -y install r-base evince 
 sudo apt-get -y autoremove
-sudo apt-get -y install --no-install-recommends tex-common texlive-base texlive-base-bin texlive-font-utils texlive-latex-extra
+
+echo Installing R packages | tee -a $log
+Rscript /home/sbl/budden2015treeome/scripts/install_packages.R
 
 echo Completed package installation | tee -a $log
